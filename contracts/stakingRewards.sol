7d608515343b71af2b2d7b2c950d5e151367a4ce
Werner Liechti <werner@saynode.ch>
2022-01-13 16:03:38 +0100

    Added owner modifier

--- contracts/stakingRewards.sol
+++ contracts/stakingRewards.sol
@@ -1,33 +1,58 @@
 
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8;
+pragma solidity ^0.8.10;
 
 contract StakingRewards {
-    ERC20 public rewardsToken;
+
     ERC20 public stakingToken;
 
-    uint public rewardRate = 100;
+    address private owner;
+
     uint public lastUpdateTime;
     uint public rewardPerTokenStored;
 
     mapping(address => uint) public userRewardPerTokenPaid;
     mapping(address => uint) public rewards;
 
-    uint private _totalSupply;
+    uint private _contractBalance = address(this).balance;
+    uint private _poolSize;
+    uint private rewardRate;
+
     mapping(address => uint) private _balances;
 
-    constructor(address _stakingToken, address _rewardsToken) {
+    constructor(address _stakingToken) {
         stakingToken = ERC20(_stakingToken);
-        rewardsToken = ERC20(_rewardsToken);
+        owner = msg.sender;
     }
 
+    modifier _ownerOnly(){
+      require(msg.sender == owner);
+      _;
+    }
+
+    function setRewardRate(uint rate) public _ownerOnly {
+
+        rewardRate = rate;
+    }
+
+    function checkRewardRate() public view returns (uint) {
+
+    return rewardRate;
+    }
+
+    function poolSize() public view returns (uint){
+
+    return _poolSize;
+    }
+
+
     function rewardPerToken() public view returns (uint) {
-        if (_totalSupply == 0) {
+        if (_contractBalance == 0) {
             return 0;
         }
         return
             rewardPerTokenStored +
-            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _totalSupply);
+            (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / _poolSize);
     }
 
     function earned(address account) public view returns (uint) {
@@ -47,13 +72,13 @@ contract StakingRewards {
     }
 
     function stake(uint _amount) external updateReward(msg.sender) {
-        _totalSupply += _amount;
+        _poolSize += _amount;
         _balances[msg.sender] += _amount;
         stakingToken.transferFrom(msg.sender, address(this), _amount);
     }
 
     function withdraw(uint _amount) external updateReward(msg.sender) {
-        _totalSupply -= _amount;
+        _poolSize-= _amount;
         _balances[msg.sender] -= _amount;
         stakingToken.transfer(msg.sender, _amount);
     }
@@ -61,12 +86,11 @@ contract StakingRewards {
     function getReward() external updateReward(msg.sender) {
         uint reward = rewards[msg.sender];
         rewards[msg.sender] = 0;
-        rewardsToken.transfer(msg.sender, reward);
+        stakingToken.transfer(msg.sender, reward);
     }
 }
 
 interface ERC20 {
-    function totalSupply() external view returns (uint);
 
     function balanceOf(address account) external view returns (uint);
 

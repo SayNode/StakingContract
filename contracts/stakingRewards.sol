3021049caaebdf975a449d72825276b70ea96b75
Werner Liechti <werner@saynode.ch>
2022-01-18 01:20:12 +0100

    Fixed reward function

--- contracts/stakingRewards.sol
+++ contracts/stakingRewards.sol
@@ -7,26 +7,25 @@ pragma solidity ^0.8.10;
 contract StakingRewards {
     ERC20 public stakingToken;
 
-    uint public rewardPercentage;
+
     uint public _poolSize;
     uint public _lockingPeriod;
     uint public contractBalance;
-    uint public rewardPeriod;
     
     address private owner;
 
     struct Stake {
         address user;
-        uint256 amount;
-        uint64 sinceBlock;
-        uint64 untilBlock;   
+        uint amount;
+        uint sinceBlock;
+        uint untilBlock;   
         uint reward;
          }
 
     struct Reward{
         uint reward;
-        uint64 start;
-        uint64 end;
+        uint start;
+        uint end;
     }
 
     Stake[] public stakes;
@@ -56,17 +55,18 @@ contract StakingRewards {
     function stake(uint _amount) external  {
         _poolSize += _amount;
         stakingToken.transferFrom(msg.sender, address(this), _amount);
-        stakes.push(Stake(msg.sender, _amount, uint64(block.timestamp), uint64(block.timestamp + _lockingPeriod ), 0));
+        stakes.push(Stake(msg.sender, _amount, uint(block.timestamp), uint(block.timestamp + _lockingPeriod ), 0));
         
     }
 
 
-    function unstake (uint256 _id) external {
+    function unstake (uint _id) external {
         require(stakes[_id].user == msg.sender, 'Not your stake');
         require(stakes[_id].amount > 0, 'Nothing to unstake');
+        require(stakes[_id].untilBlock < uint(block.timestamp), 'To early');
 
         uint _amount = stakes[_id].amount;
-        stakes[_id].untilBlock= uint64(block.timestamp);
+        stakes[_id].untilBlock= uint(block.timestamp);
 
         calculateReward(_id); // Calculate Reward of the Stake
 
@@ -74,34 +74,38 @@ contract StakingRewards {
         _poolSize-= _amount;
         
         stakingToken.transfer(msg.sender, _amount);
+        getReward(_id);
         
     }
 
-    function calculateReward(uint256 _id) public {
-        uint64 end;
+    function calculateReward(uint _id) public {
+        stakes[_id].reward =0;
+        uint end;
+
         
-        if(stakes[_id].untilBlock> uint64(block.timestamp)){
-            end =uint64(block.timestamp);
+        if(stakes[_id].untilBlock> uint(block.timestamp)){
+            end = uint(block.timestamp);
         }
         else{
             end = stakes[_id].untilBlock;
         }
-
-        for (uint i=0; i<=rewards.length; i++){
-            if(rewards[i].end > stakes[_id].sinceBlock ){
-                stakes[_id].reward = rewards[i].end - stakes[_id].sinceBlock * rewards[i].reward * stakes[_id].amount; // finds the  Start of the staking time
+        
+         for (uint i=0; i<rewards.length; i++){
+             
+            if ((rewards[i].start < stakes[_id].sinceBlock)  && (rewards[i].end == 0)){
+                stakes[_id].reward += (end - stakes[_id].sinceBlock) * rewards[i].reward * stakes[_id].amount; // if staking starts and ends without reward change
             }
 
-            else if ((rewards[i].start > stakes[_id].sinceBlock) && (rewards[i].end > end) && (rewards[i].end != 0)  ){
-                stakes[_id].reward += end - stakes[_id].sinceBlock * rewards[i].reward * stakes[_id].amount; // finds Regions where the reward Rate change during staking
+            else if((rewards[i].start < stakes[_id].sinceBlock) && (rewards[i].end > stakes[_id].sinceBlock) && (rewards[i].end != 0)){
+                stakes[_id].reward += (rewards[i].end - stakes[_id].sinceBlock) * rewards[i].reward * stakes[_id].amount; //first feward rate
             }
 
-            else if ((rewards[i].start < end&& rewards[i].start > stakes[_id].sinceBlock  )  && (rewards[i].end == 0)){
-                stakes[_id].reward += end - rewards[i].start * rewards[i].reward * stakes[_id].amount; // finds the end of the staking time
+            else if ((rewards[i].start > stakes[_id].sinceBlock) && (rewards[i].end < end) && (rewards[i].end != 0)  ){
+                stakes[_id].reward += (rewards[i].end - rewards[i].start) * rewards[i].reward * stakes[_id].amount; //reward rate during staking
             }
 
-            else if ((rewards[i].start < stakes[_id].sinceBlock)  && (rewards[i].end == 0)){
-                stakes[_id].reward += end - stakes[_id].sinceBlock * rewards[i].reward * stakes[_id].amount; // if the staking was in a period where no changes to the Rewardrate apperared, this will be executed
+            else if ((rewards[i].start < end) && (rewards[i].start > stakes[_id].sinceBlock)  && (rewards[i].end == 0)){
+                stakes[_id].reward += (end - rewards[i].start) * rewards[i].reward * stakes[_id].amount; //last reward rate
             }
 
         }
@@ -109,28 +113,32 @@ contract StakingRewards {
 
     }
 
+    function getReward(uint _id) public {
 
+        require(stakes[_id].user == msg.sender, 'Not your reward');
+        require(stakes[_id].reward > 0, 'Nothing to unstake');
+        require(stakes[_id].reward < contractBalance , 'Not enought balance in contract');
 
-    function getReward(uint256 _id) external {
-
-        require(stakes[_id].user == msg.sender, 'Not your stake');
-        require(stakes[_id].amount == 0, 'Nothing to unstake');
-
+        stakes[_id].reward = 0;
         stakingToken.transfer(msg.sender, stakes[_id].reward);
  
     }
 
-
     function setRewardRate(uint rate) public _ownerOnly {
         
-        rewards.push(Reward(rate, uint64(block.timestamp), 0));
-        rewards[rewards.length -1].end = uint64(block.timestamp);
-        
+        rewards.push(Reward(rate, uint(block.timestamp), uint(0)));
+        if(rewards.length>1){
+            rewards[rewards.length -2].end = uint(block.timestamp);
+        }
         
     }  
     
     function setLockingPeriod(uint lockingPeriod) public _ownerOnly {
         _lockingPeriod = lockingPeriod;
+    }
+
+    function untilLockingEnd(uint _id) public view returns(uint) {
+        return stakes[_id].untilBlock - uint(block.timestamp);
     }
 
 }
